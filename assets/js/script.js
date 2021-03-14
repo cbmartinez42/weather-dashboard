2202a62146fca1f6fec0aff5f5202641d31977b0
Chris Martinez <cbmartinez42@gmail.com>
2021-03-13 20:05:08 -0600

    updating with working weather functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,28 @@
-function init() {
-
-let searchInputEl = document.getElementById('search-input');
-let searchBtn = document.getElementById('search-btn');
-let today = moment().format('dddd, MMM Do, YYYY');
-console.log(today);
-
 let searchHandler = function (event){
     event.preventDefault();
     let city = searchInputEl.value.trim();
-
     if (city) {
-        getCurrentWeather(city);
-        getFiveDay(city);
+        
+        getLatLon(city);
+        // getFiveDay(city);
       } else {
         alert('Please enter a city');
       }
 }
 
-let getCurrentWeather = function (city) {
+// function init() {
+
+let searchInputEl = document.getElementById('search-input');
+let searchBtn = document.getElementById('search-btn');
+let today = moment().format('dddd, MMM Do, YYYY');
+console.log(today);
+// let cityValue = searchInputEl.value.trim();
+// console.log(cityValue);
+
+
+
+let getLatLon = function (city) {
+    
     let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=3cbac659165d03c7a5a56ef38b21d47f';
   
     fetch(apiUrl)
@@ -25,9 +30,11 @@ let getCurrentWeather = function (city) {
             if (response.ok) {
             console.log(response);
             response.json().then(function (data) {
+
                 console.log(data);
-                displayWeather(data); // data, city
-                // console.log(response.data.weather[0].icon);
+                // const city = data.name;
+                createCurrWeatherDiv(data);
+                handleOneCall(data); // data, city
             });
             } else {
             alert('Error: ' + response.statusText);
@@ -36,61 +43,15 @@ let getCurrentWeather = function (city) {
         .catch(function (error) {
             alert('Unable to connect to Weather Database');
         });
- 
-  };
+   };
 
-let getFiveDay = function (city) {
-    let apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=3cbac659165d03c7a5a56ef38b21d47f';
-
-    fetch(apiUrl)
-        .then(function (response) {
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function (data) {
-                const today = new Date()
-                const tomorrow = new Date(today)
-                tomorrow.setDate(tomorrow.getDate() + 1)
-            console.log(data);
-            const temp = []
-            for (const node of data.list){
-                const date = node.dt_txt.substring(0,10)
-                const formattedDate = tomorrow.toISOString().substring(0,10)
-                // console.log(date);
-                // console.log(formattedDate);
-                if (date == formattedDate) {
-                    temp.push(node)
-                }  
-            }
-            console.log(temp)
-            const value = temp.pop();   // need to hardcode for the 6th index
-            displayFiveDay(value);
-            });
-        } else {
-            alert('Error: ' + response.statusText);
-        }
-        })
-        .catch(function (error) {
-            alert('Unable to connect to Weather Database');
-        });
-};
-
-// humidity wind speed uv index
-let displayWeather = function (data) {
+function createCurrWeatherDiv(data) {
     const currentWeatherContainer = $('#currWeatherContainer');
-    const cityNm = data.name;
-    const temp = data.main.temp + '°';
-    const icon = data.weather[0].icon;
-    const humidity = data.main.humidity + '%';
-    const windSpeed = data.wind.speed;
-
+    const city = data.name;
     let currentWeatherBody = $('<div>')
-        .addClass('card-body')
-        .html(`<h4 class="card-title">${cityNm}</h4><h5>${today}</h5> 
-        <h4><img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather Image"></h4>
-        <h5 class="card-subtitle mb-2">Temperature: ${temp}</h5>
-        <p id='currHumidity'>Humidity: ${humidity}</p>
-        <p id='wind'>Wind Speed: ${windSpeed}MPH</p>
-        <p id='uvIndex'>UV Index: ${humidity}</p>`);   // <---- need to figure this out
+        // .attr('id)
+        .addClass('card-body current-weather-body')
+        .html(`<h4 class="card-title">${city}</h4>`);
     let currWeatherCard = $('<div>')
         .addClass('card');
     let currWeatherCol = $('<div>')
@@ -103,26 +64,81 @@ let displayWeather = function (data) {
     currentWeatherContainer.append(currWeatherRow);
 }
 
-let displayFiveDay = function(value) {
-    console.log(value);
-    const fiveDayContainer = $('.five-day-cards');
-    const temp = data.main.temp + '°';
-    const humidity = data.main.humidity + '%';
+function handleOneCall(data){
+    const cityNm = data.name;
+    const lat = data.coord.lat;
+    const lon = data.coord.lon;
+    console.log(lat);
+    console.log(lon);
+    let oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=hourly,minutely,alerts&appid=3cbac659165d03c7a5a56ef38b21d47f';
+    fetch(oneCallUrl)
+        .then(function (response) {
+            if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                const city = cityNm
+
+                console.log(data);
+                displayWeather(data); // data, city
+                displayFiveDay(data);
+            });
+            } else {
+            alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to Weather Database');
+        });
+   };
+   
+
+// humidity wind speed uv index
+let displayWeather = function (data) {
+    const temp = data.current.temp + '°';
+    const icon = data.current.weather[0].icon;
+    const humidity = data.current.humidity + '%';
+    const windSpeed = data.current.wind_speed;
+    const uvIndex = data.current.uvi;
+
+    $(`<h5>${today}</h5> 
+    <h4><img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather Image"></h4>
+    <h5 class="card-subtitle mb-2">Temperature: ${temp}</h5>
+    <p id='currHumidity'>Humidity: ${humidity}</p>
+    <p id='wind'>Wind Speed: ${windSpeed}MPH</p>
+    <p id='uvIndex'>UV Index: ${uvIndex}</p>`).appendTo('.current-weather-body')
+}
+
+// need date, icon, temp, humidity
+function displayFiveDay (data) {
+    const fiveDayRow = $('.five-day-cards');
+  
+    for (let i = 1; i < 6; i++) {
+ 
+    let unixDate = data.daily[i].dt
+    let date = moment.unix(unixDate).format('M/D/YY');
+    let temp = data.daily[i].temp.max + '°';
+    let humidity = data.daily[i].humidity + '%';
+    let icon = data.daily[i].weather[0].icon
+    console.log(icon)
 
     let fiveDayBody = $('<div>')
         .addClass('card-body')
-        .html(`<h5 class="card-title">Temperature: ${temp} <img src="http://placekitten.com/30/30" alt="Weather Image"></h5>
+        .html(`<h5 class="card-title">${date}</h5>
+        <h6><img src="https://openweathermap.org/img/wn/${icon}.png" alt="Weather Image"></h6>
         <h6 class="card-subtitle">${temp}</h6>
         <p>Humidity: ${humidity}</p>`)
     let fiveDayCard = $('<div>')
         .addClass('card');
-    let fiveDayCol = $('div')
-        addClass('col-2');
+    let fiveDayCol = $('<div>')
+        .addClass('col-2');
     fiveDayCard.append(fiveDayBody);
     fiveDayCol.append(fiveDayCard);
-    fiveDayContainer.append(fiveDayCol);
+    fiveDayRow.append(fiveDayCol)
+    console.log('line 137')
+    }
+    // rowContainer.append(fiveDayCol)
 }
 
-  $('#search-btn').click(searchHandler);
-}
-init()
+$('#search-btn').click(searchHandler);
+// }
+// init()
